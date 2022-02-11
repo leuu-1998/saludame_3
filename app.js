8fc5088c493e28785b9ef127596ea24f40f3face
leuu-1998 <lmoinah@uni.pe>
2022-02-10 21:43:48 -0500

    saludo implementado

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/formP.html");
+});
+
+app.post("/", (req, res) => {
+  const { nombre } = req.body;
+
+  res.send(`
+  <div> 
+    <h1>Hola ${nombre}</h1>
+    </div>
+  `);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000!!!"));
